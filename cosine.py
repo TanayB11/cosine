47f6fb7103e2825e3d395cc438824fef2e6a8072
Tanay Biradar <34867487+TanayB11@users.noreply.github.com>
2023-08-07 00:44:36 -0700

    Prettify search responses and stream file upload

--- cosine.py
+++ cosine.py
@@ -1,6 +1,8 @@
 import os, argparse
 import requests
 import shutil, json
+from tabulate import tabulate
+import textwrap
 
 API_URL = 'http://localhost:8000/api'
 VAULT_LOCATION = '/Users/tanay/Documents/Obsidian/Personal/Notes/'
@@ -11,12 +13,13 @@ def upload(vault_location=VAULT_LOCATION) -> None:
     shutil.make_archive(zip_filename, 'zip', vault_location)
 
     # Make POST request to upload zip file
-    files=[ ('directory', (f'{zip_filename}.zip', open(f'{zip_filename}.zip','rb'), 'application/zip')) ]
+    with open(f'{zip_filename}.zip','rb') as zipped: # streamed for large files
+        files=[ ('directory', (f'{zip_filename}.zip', zipped, 'application/zip')) ]
 
-    response = requests.post(f'{API_URL}/reindex/', files=files)
+        response = requests.post(f'{API_URL}/reindex/', files=files)
 
-    if response.status_code != 200:
-      print('Upload failed with status code:', response.status_code)
+        if response.status_code != 200:
+          print('Upload failed with status code:', response.status_code)
 
     # Delete zip file
     os.remove(f'{zip_filename}.zip')
@@ -30,7 +33,7 @@ def search(query: str):
       print('Upload failed with status code:', response.status_code)
 
     # print(json.loads(response.text)['message']['documents'][0][0])
-    return json.loads(response.text)['message']['documents'][0]
+    return json.loads(response.text)['message']
 
 def main():
     parser = argparse.ArgumentParser()
@@ -44,9 +47,23 @@ def main():
     elif args.command == "search":
         if args.query:
             results = search(args.query)
-            print(results[0])
+
+            term_width = os.get_terminal_size().columns
+            messages = [
+                {
+                    "File": results['metadatas'][0][idx]['source'].split('/')[-1],
+                    "Text": textwrap.fill(message, width=int(term_width * 2//3))
+                }
+              for (idx, message) in enumerate(results['documents'][0])
+            ]
+            headers = {"ID": "ID", "Message": "Message"}
+            table = tabulate(messages, headers=headers, tablefmt="grid")
+            print(table, "\n")
+
+            # print(results[''][0])
         else:
             print("No search query provided")
 
 if __name__ == '__main__':
     main()
+
